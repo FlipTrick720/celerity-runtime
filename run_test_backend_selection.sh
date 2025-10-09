5e75cfe93f1ecac5595d0d4ce911c2c97f868450
Malte <malte.braig@student.uibk.ac.at>
2025-10-09 18:49:40 +0200

    exclude testing scripts

--- run_test_backend_selection.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source /opt/intel/oneapi/setvars.sh
-
-# Compile the backend selection debug tool
-icpx -fsycl -std=c++20 -DSYCL_EXT_ONEAPI_BACKEND_LEVEL_ZERO test_backend_selection.cc -o test_backend_selection
-
-# Run it with the same environment
-./test_backend_selection
