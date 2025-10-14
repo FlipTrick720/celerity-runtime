ba2074f49ebf66130a67acc380e299046dd57662
Malte <malte.braig@student.uibk.ac.at>
2025-10-14 18:08:01 +0200

    unimportant changes

--- run_bench.sh
+++ run_bench.sh
@@ -5,13 +5,15 @@ export UR_DISABLE_ADAPTERS=OPENCL
 
 # Run 1
 taskset -c 0-15 ./scripts/run_matrix.sh results_$(date +%Y%m%d_%H%M%S)
+echo "=== Cool Down ==="
+sleep 300 #(Cool Down Phase)
 
 # Run 2
+taskset -c 0-15 ./scripts/run_matrix.sh results_$(date +%Y%m%d_%H%M%S)
 echo "=== Cool Down ==="
 sleep 300 #(Cool Down Phase)
-taskset -c 0-15 ./scripts/run_matrix.sh results_$(date +%Y%m%d_%H%M%S)
 
 # Run 3
+taskset -c 0-15 ./scripts/run_matrix.sh results_$(date +%Y%m%d_%H%M%S)
 echo "=== Cool Down ==="
 sleep 300 #(Cool Down Phase)
-taskset -c 0-15 ./scripts/run_matrix.sh results_$(date +%Y%m%d_%H%M%S)
