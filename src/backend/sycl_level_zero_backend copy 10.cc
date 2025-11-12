320a5a1fcfc998c49e2916ebae2561f4aa4d9187
Malte <malte.braig@student.uibk.ac.at>
2025-11-12 13:49:55 +0100

    fix copy 10

--- src/backend/sycl_level_zero_backend copy 10.cc
+++ src/backend/sycl_level_zero_backend copy 10.cc
@@ -431,6 +431,8 @@ sycl_level_zero_backend::sycl_level_zero_backend(const std::vector<sycl::device>
 	}
 }
 
+sycl_level_zero_backend::~sycl_level_zero_backend() = default;
+
 async_event sycl_level_zero_backend::enqueue_device_copy(device_id device, size_t device_lane, const void* const source_base, void* const dest_base,
     const region_layout& source_layout, const region_layout& dest_layout, const region<3>& copy_region, const size_t elem_size) //
 {
